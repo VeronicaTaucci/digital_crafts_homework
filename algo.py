cea61f5aee7312634c0e77c8a1d2dc9e43e92315
VeronicaTaucci <taucci.veronica@gmail.com>
2022-02-24 18:54:01 -0500

    new updates

---
+++ algo.py
@@ -0,0 +1,24 @@
+num = int(input("What is the number?"))
+
+if num % 3 == 0 and num % 5 == 0:
+    print("Fizz Buzz")
+elif num % 3 == 0:
+    print("Fizz")
+elif num % 5 == 0:
+    print("Buzz")
+
+array = [12, 3, 1, 2, -6, 5, -8, 6]
+
+# sort_arr = [-8,-6,1,2,3,5,6,12]
+
+def sum_nums(array,target_sum):
+    array.sort() 
+    empty_array=[]
+    for num1 in array:
+        num2 = num1+1
+        num3 = len(array - 1)
+        
+        
+            
+        
+    
